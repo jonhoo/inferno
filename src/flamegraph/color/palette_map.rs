11fc56624a4c178640afb3819dd23a4bc8778008
Jordi <jchauzi@upmem.com>
2019-03-08 21:58:19 +0100

    Implementing interface functions for PaletteMap

--- src/flamegraph/color/palette_map.rs
+++ src/flamegraph/color/palette_map.rs
@@ -9,9 +9,88 @@ use std::io::Write;
 use std::path::Path;
 use std::str::FromStr;
 
-pub(crate) struct PaletteMap<'a>(HashMap<Cow<'a, str>, (u8, u8, u8)>);
+#[derive(Default)]
+pub struct PaletteMap<'a>(HashMap<Cow<'a, str>, (u8, u8, u8)>);
 
 impl<'a> PaletteMap<'a> {
+    pub fn new() -> Self {
+        Default::default()
+    }
+
+    pub fn get(&self, func: &str) -> Option<(u8, u8, u8)> {
+        self.0.get(func).cloned()
+    }
+
+    pub fn insert(&mut self, func: &'a str, color: (u8, u8, u8)) -> Option<(u8, u8, u8)> {
+        self.0.insert(Cow::from(func), color)
+    }
+
+    pub fn iter(&self) -> impl Iterator<Item = (&str, (u8, u8, u8))> {
+        self.0.iter().map(|(func, color)| (func.as_ref(), *color))
+    }
+
+    pub fn from_stream(reader: &mut dyn io::Read) -> io::Result<Self> {
+        let mut map = HashMap::default();
+        let reader = BufReader::new(reader);
+
+        for line in reader.lines() {
+            let line = line?;
+
+            // A line is formatted like this: NAME -> rbg(RED, GREEN, BLUE)
+            let mut words = line.split("->");
+
+            let name = match words.next() {
+                Some(name) => name,
+                None => return Err(io::Error::from(io::ErrorKind::InvalidInput)),
+            };
+
+            let color = match words.next() {
+                Some(name) => name,
+                None => return Err(io::Error::from(io::ErrorKind::InvalidInput)),
+            };
+
+            if words.next().is_some() {
+                return Err(io::Error::from(io::ErrorKind::InvalidInput));
+            }
+
+            let rgb_color = parse_rgb_string(color)
+                .ok_or_else(|| io::Error::from(io::ErrorKind::InvalidData))?;
+            map.insert(Cow::from(name.to_string()), rgb_color);
+        }
+
+        Ok(PaletteMap(map))
+    }
+
+    pub fn to_stream(&self, writer: &mut dyn io::Write) -> io::Result<()> {
+        let mut entries = self.0.iter().collect::<Vec<_>>();
+        // We sort the palette because the Perl implementation does.
+        entries.sort_unstable();
+
+        for (name, color) in entries {
+            writer.write_all(
+                format!("{}->rgb({},{},{})\n", name, color.0, color.1, color.2).as_bytes(),
+            )?
+        }
+
+        Ok(())
+    }
+
+    pub fn load_from_file(path: &dyn AsRef<Path>) -> io::Result<Self> {
+        // If the file does not exist, it is probably the first call to flamegraph with a consistent
+        // palette: there is nothing to load.
+        if path.exists() {
+            let mut file = File::open(path)?;
+            PaletteMap::from_stream(&mut file)
+        } else {
+            Ok(PaletteMap::default())
+        }
+    }
+
+    pub fn save_to_file(&self, path: &dyn AsRef<Path>) -> io::Result<()> {
+        let mut file = OpenOptions::new().write(true).create(true).open(path)?;
+        self.to_stream(&mut file)
+    }
+
     pub fn load(file: &str) -> quick_xml::Result<Self> {
         let mut map = HashMap::default();
         let path = Path::new(file);
