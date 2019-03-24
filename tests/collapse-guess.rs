d0f049b01c27079f76ae67ec03286f9ceebad5c8
Jason Rodney Hansen <jasonrodneyhansen@gmail.com>
2019-03-24 12:13:23 -0600

    Add CLI tests using assert_cmd

--- tests/collapse-guess.rs
+++ tests/collapse-guess.rs
@@ -5,10 +5,13 @@ extern crate inferno;
 
 mod collapse_common;
 
+use assert_cmd::prelude::*;
 use collapse_common::*;
 use inferno::collapse::guess::Folder;
 use log::Level;
-use std::io;
+use std::fs::File;
+use std::io::{self, BufReader, Cursor};
+use std::process::{Command, Stdio};
 
 fn test_collapse_guess(test_file: &str, expected_file: &str) -> io::Result<()> {
     test_collapse(Folder {}, test_file, expected_file)
@@ -103,4 +106,33 @@ fn collapse_guess_invalid_perf_should_log_error() {
             );
         },
     );
+}
+
+#[test]
+fn collapse_guess_cli() {
+    let input_file = "./tests/data/collapse-dtrace/java.txt";
+    let expected_file = "./tests/data/collapse-dtrace/results/java.txt";
+
+    // Test with file passed in
+    let output = Command::cargo_bin("inferno-collapse-guess")
+        .unwrap()
+        .arg(input_file)
+        .output()
+        .expect("failed to execute process");
+    let expected = BufReader::new(File::open(expected_file).unwrap());
+    compare_results(Cursor::new(output.stdout), expected, expected_file);
+
+    // Test with STDIN
+    let mut child = Command::cargo_bin("inferno-collapse-guess")
+        .unwrap()
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .spawn()
+        .expect("Failed to spawn child process");
+    let mut input = BufReader::new(File::open(input_file).unwrap());
+    let stdin = child.stdin.as_mut().expect("Failed to open stdin");
+    io::copy(&mut input, stdin).unwrap();
+    let output = child.wait_with_output().expect("Failed to read stdout");
+    let expected = BufReader::new(File::open(expected_file).unwrap());
+    compare_results(Cursor::new(output.stdout), expected, expected_file);
 }
