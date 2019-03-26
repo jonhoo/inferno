f8a6a9248a5bbaa1672a458b2c4dfbcba9e02abf
Jason Rodney Hansen <jasonrodneyhansen@gmail.com>
2019-03-25 21:18:56 -0600

    Combine tests

--- tests/flamegraph.rs
+++ tests/flamegraph.rs
@@ -233,22 +233,6 @@ fn flamegraph_nameattr_empty_attribute() {
 }
 
 #[test]
-fn flamegraph_nameattr_href_no_target() {
-    let input_file = "./flamegraph/test/results/perf-cycles-instructions-01-collapsed-all.txt";
-    let expected_result_file = "./tests/data/flamegraph/nameattr/nameattr_href_no_target.svg";
-    let nameattr_file = "./tests/data/flamegraph/nameattr/nameattr_href_no_target.txt";
-
-    let options = flamegraph::Options {
-        hash: true,
-        func_frameattrs: flamegraph::FuncFrameAttrsMap::from_file(&PathBuf::from(nameattr_file))
-            .unwrap(),
-        ..Default::default()
-    };
-
-    test_flamegraph(input_file, expected_result_file, options).unwrap();
-}
-
-#[test]
 fn flamegraph_nameattr_duplicate_attributes() {
     let input_file = "./flamegraph/test/results/perf-cycles-instructions-01-collapsed-all.txt";
     let expected_result_file = "./tests/data/flamegraph/nameattr/nameattr_duplicate_attributes.svg";
