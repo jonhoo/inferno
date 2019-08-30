52236531b46e1bb33876b59e9c4b99be21e0758f
Jakub Beránek <berykubik@gmail.com>
2019-08-30 15:23:51 +0200

    Add fuzzing tests

---
+++ fuzz/fuzz_targets/guess.rs
@@ -0,0 +1,8 @@
+#![no_main]
+
+use inferno::collapse::{Collapse, guess::Folder};
+use libfuzzer_sys::fuzz_target;
+
+fuzz_target!(|data: &[u8]| {
+    Folder::default().collapse(data, std::io::sink()).ok();
+});
