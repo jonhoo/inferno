8d4cf046869ffadfe9d0963a99bb4acbc19abb6d
Mike Roberts <mike.roberts.2k10@googlemail.com>
2022-05-08 12:09:07 +0100

    Add .trim() before line filtering to catch whitespace-only lines

--- src/flamegraph/mod.rs
+++ src/flamegraph/mod.rs
@@ -394,7 +394,10 @@ where
     W: Write,
 {
     let mut reversed = StrStack::new();
-    let lines = lines.into_iter().filter(|line| !(line.is_empty() || line.starts_with("# ")));
+    let lines = lines
+        .into_iter()
+        .map(|line| line.trim())
+        .filter(|line| !(line.is_empty() || line.starts_with("# ")));
 
     let (mut frames, time, ignored, delta_max) = if opt.reverse_stack_order {
         if opt.no_sort {
